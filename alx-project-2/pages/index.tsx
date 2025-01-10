4fcdcc3171d9d21854fa20bfbf1c2653ff83013e
amuMabirimise <awmabirimise@gmail.com>
2025-01-10 11:50:44 +0200

    Set Up a Next.js Project

--- alx-project-2/pages/index.tsx
+++ alx-project-2/pages/index.tsx
@@ -1,6 +1,14 @@
+import React from "react";
+
 const Home = () => (
-  <div>
-    <h1>Welcome to the ALX Next.js Project!</h1>
+  <div className="flex flex-col items-center justify-center min-h-screen p-4 bg-gray-100">
+    <h1 className="text-4xl font-bold text-blue-600">
+      Welcome to the ALX Next.js Project!
+    </h1>
+    <p className="mt-4 text-lg text-gray-700">
+      This is the starting point of your Next.js journey with TypeScript and
+      Tailwind CSS.
+    </p>
   </div>
 );
 
